f77455cda2794be55580d541f65762ec611fdd26
pallavireddy20 <pathikondapallavi9108@gmail.com>
2025-09-01 21:42:04 +0530

    Add files via upload

---
+++ var.js
@@ -0,0 +1,18 @@
+//Declare a variable age as a string with value "25".
+//Convert it into a number and print the result.
+let age = "25";     
+let numAge = Number(age);  
+console.log(numAge);       
+console.log(typeof numAge);
+
+// Declare two variables:
+// num1 = "10" (string)
+// num2 = 20 (number)
+// Convert both to numbers, add them, and print the sum.
+
+var num1 = "10"
+console.log(typeof(num1))
+var num2 = Number(num1)
+console.log(num2)
+var num3 = 20
+console.log( "num2 + num3")
