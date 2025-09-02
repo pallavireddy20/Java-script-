35a0dc9ccb387859a5e9d78d7e6fcbeae0abbfc6
pallavireddy20 <pathikondapallavi9108@gmail.com>
2025-09-02 21:03:35 +0530

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,14 @@
+// Write a program to check if a number is even or odd using the modulus (%) operator.
+// Example: Input = 7 → Output: Odd
+let a = 7
+if(a % 2==0){
+   console.log(a+"is even")
+}
+else{
+    console.log(a +" is odd")
+}
+
+let b = 5
+let c = 7
+let result = (b>c)? b : c;
+console.log("the largest number is :" + result)
