4ec500f5d8868a5fc3ade7d87b4918f742f6c7fb
pallavireddy20 <pathikondapallavi9108@gmail.com>
2025-12-02 20:36:34 +0530

    Add files via upload

---
+++ problem1.js
@@ -0,0 +1,32 @@
+//two numbers without using a third variable
+   //by using airthmetic/sub
+// let a = 5;
+// let b = 3;
+// a = a+b;
+// b = a-b;
+// a = a-b;
+// console.log(a,b)
+// by using Xor method
+// let a = 5;
+// let b = 3;
+// a=a^b;
+// b=a^b;
+// a=a^b;
+// console.log(a,b)
+
+// let a = 5;
+// let b = 3;
+// [a,b] = [b,a];
+// console.log(a,b);
+
+// Example — run in browser console or Node.js
+let studentName = "Pallavi";
+let age = 20;
+let isMember = true;
+
+console.log("Name:", studentName);
+console.log("Age:", age);
+console.log("Is member?:", isMember);
+
+// Print types
+console.log("Types ->", typeof studentName, typeof age, typeof isMember);
