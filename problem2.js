55a0166807c499ab01d2e0c76d1dfd6eab53b231
pallavireddy20 <pathikondapallavi9108@gmail.com>
2025-12-03 17:09:48 +0530

    Add files via upload

---
+++ problem2.js
@@ -0,0 +1,20 @@
+//sum of two numbers
+// let a = 5;
+// let b = 3;
+// let sum = a+b;
+// console.log("sum is :",sum);
+
+       //even or odd 
+// let a = 6;
+// if(a %2==0){
+//     console.log("a is even");
+// }
+// else{
+//     console.log("a is odd");
+// }
+
+let a = 5;
+let b = 3;
+let remainder = a % b;
+console.log("remainder =",remainder);
+
---
+++ problem4.js
@@ -0,0 +1,11 @@
+// let name = "pallavi"
+// let age = "20"
+// let city = "Hyderabad"
+// console.log(name,age,city);
+
+let a = 20;
+let b = 30;
+let temp = a;
+a=b;
+b=temp;
+console.log(a,b);
