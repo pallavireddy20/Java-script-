2070c136ce775130d60a7df23d1d24e290922021
pallavireddy20 <pathikondapallavi9108@gmail.com>
2025-12-04 20:33:49 +0530

    Add files via upload

---
+++ problem9.js
@@ -0,0 +1,20 @@
+// sum of two numbers
+let num1=20;
+let num2=30;
+let sum=num1+num2;
+console.log(sum);
+
+// convert 45 into String
+
+let num=10;
+let str=String(num)
+console.log(str);
+console.log(typeof str);
+
+//swaping two numbers without using third variable
+let a=10;
+let b=20;
+[a,b]=[b,a];
+console.log(a,b);
+
+console.log(parseFloat(3.1415));
