70910b75f7581e29160c8cbbd7dd04a8a641b3f7
pallavireddy20 <pathikondapallavi9108@gmail.com>
2025-12-04 20:26:14 +0530

    Add files via upload

---
+++ problem8.js
@@ -0,0 +1,15 @@
+// converting string into number
+
+// let num="10"
+// console.log(Number(num));
+
+// convert mintues into seconds
+
+//  let minutes = 5;
+//  let seconds = minutes*60;
+//  console.log(seconds);
+
+// converting celsius into fahrenhit
+ let C = 20;
+ let F =(C*9/2)+32;
+ console.log(F);
