bdcfd76c205419fbddb6f0d06f08ab477836aeb8
pallavireddy20 <pathikondapallavi9108@gmail.com>
2025-12-04 20:38:39 +0530

    Add files via upload

---
+++ problem10.js
@@ -0,0 +1,11 @@
+// input two numbers (strings), convert both to numbers, and print their sum.
+let num1="10";
+let num2="20";
+let num3=Number(num1);
+let num4=Number(num2);
+console.log(num3+num4);
+
+let x="40";
+let y=5;
+let x1=Number(x);
+console.log(x1/y);
