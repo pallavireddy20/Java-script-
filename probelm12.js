1ba7b4f3a819695e3f9d76517b1c35095a00300b
pallavireddy20 <pathikondapallavi9108@gmail.com>
2025-12-05 20:35:39 +0530

    Add files via upload

---
+++ probelm12.js
@@ -0,0 +1,46 @@
+//conditions
+// even or odd
+// let a=8;
+// if(a%2==0){
+//     console.log("a is evennumber");
+// }
+// else{
+//     console.log("a is oddnumber");
+// }
+
+//Given 3 numbers, print the largest number.
+// let a=4;
+// let b=5;
+// let c=6;
+//  let largest;
+//  if(a>=b && a>=c){
+//     largest =a;
+//  }
+//  else if(b>=a && b>=c){
+//     largest =b;
+//  }
+//  else{
+//     largest=c;
+//  }
+//  console.log("largest number is:", largest);
+
+ //Write a program to check if a number is positive, negative, or zero
+
+//  let num=-5;
+//  if(num>0){
+//     console.log("positive");
+//  }
+//  else if(num<0){
+//     console.log("negative");
+//  }
+//  else{
+//     console.log("zero");
+//  }
+
+//Check if a person’s age is between 13 and 19 (Teenager or Not).
+let age=20;
+if(age>=13 && age<=19){
+    console.log("Teenager");
+}else{
+    console.log("Not a teenager");
+}
