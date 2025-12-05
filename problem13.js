bfbfcaeec7337dd5a2e916ef263ebee392786d89
pallavireddy20 <pathikondapallavi9108@gmail.com>
2025-12-05 20:35:55 +0530

    Add files via upload

---
+++ problem13.js
@@ -0,0 +1,19 @@
+//Create an array of 5 fruits and print the first and last fruit.
+
+// let arr=["mango","banana","kivi","orange","watermelon"]
+// let firstarr=arr[0];
+// let lastarr=arr[arr.length-1];
+// console.log(firstarr);
+// console.log(lastarr);
+
+//Add "mango" to the array and print the result.
+
+// let arr=["banana","kivi","orange","watermelon"]
+
+// arr.push("mango");
+// console.log(arr);
+//Given [1, 2, 3, 4, 5], print each number using a loop.
+let arr=[1,2,3,4,5]
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
