912cf55f131e8179a3481ea2fcb0822b3bab2a04
pallavireddy20 <pathikondapallavi9108@gmail.com>
2025-12-05 20:04:17 +0530

    Add files via upload

---
+++ problem11.js
@@ -0,0 +1,17 @@
+//convert string intoo uppercase
+
+// let s="hello world"
+// let app=s.toUpperCase();
+// console.log(app);
+
+//length of a string
+
+// let str="Bujji";
+// let len=str.length;
+// console.log(len);
+
+let str="Pallavi";
+let first=str[0];
+let last = str[str.length-1];
+console.log(first);
+console.log(last);
