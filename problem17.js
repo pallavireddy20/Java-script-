24b98d8fbee7c780db93fa59130af813a1e5b836
pallavireddy20 <pathikondapallavi9108@gmail.com>
2025-12-09 21:54:24 +0530

    Add files via upload

---
+++ problem17.js
@@ -0,0 +1,45 @@
+//Check if a number is positive or negative.
+let num=10;
+if(num>0){
+    console.log("PositiveNumber");
+}
+else{
+    console.log("NegativeNumber");
+}
+
+//Check if a given age is eligible to vote (18+).
+let age=20;
+if(age>=18){
+    console.log("Eligible to vote")
+}
+else{
+    console.log("Not eligible to vote")
+}
+
+//Check whether a number is even or odd.
+let num1=20;
+if(num1%2==0){
+    console.log("Even Number");
+}
+else{
+    console.log("Odd Number");
+}
+
+//If a number is greater than 100, print “Big Number”, otherwise print “Small Number”
+let a=10;
+if(a>100){
+    console.log("Large number")
+}
+else{
+    console.log("Small number")
+}
+
+//Check if two numbers are equal or not.
+let x=20
+let y=30
+if(x==y){
+    console.log("Numbers are equal")
+}
+else{
+    console.log("NUmbers are not equal")
+}
