b3c0db1b555de3882d17d542486bd1db5b18d499
pallavireddy20 <pathikondapallavi9108@gmail.com>
2025-12-14 20:20:08 +0530

    Add files via upload

---
+++ Arrays.js
@@ -0,0 +1,45 @@
+
+//array of numbers
+var a =[1,2,3,4,5]  // 64bits *length = 320bits
+console.log(a)
+ 
+console.log(a[2]) //3
+
+// methods of array
+console.log(a.length)
+
+var myArray = new Array()
+console.log(myArray)
+myArray.push(1)
+myArray.push(2)
+myArray.push(3)
+myArray.push(4)
+console.log(myArray)
+myArray.unshift(0)
+console.log(myArray)
+myArray.pop(4)
+console.log(myArray)
+myArray.shift()
+console.log(myArray)
+myArray.indexOf(2)
+console.log(myArray.indexOf(2))
+
+//array string numbers
+var b =["a","b","c","d","e"]
+console.log(b)
+console.log(b[4]) //e
+//array of object
+var c =[
+    {name:"pallavi" ,age: 20}
+   //{name:"pandu" ,age: 21}
+   // {name:"naveen" ,age: 19}
+]
+console.log(c)
+console.log(c[1])
+console.log(c[1].age)
+
+
+
+
+
+
